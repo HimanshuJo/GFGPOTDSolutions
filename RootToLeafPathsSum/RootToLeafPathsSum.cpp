4ce0194d16058e0030d1d74ca4b669a92114c9e5
Himanshu Joshi <himanshhjoshi@outlook.com>
2024-11-06 15:13:44 +0100

    root to leaf paths sum

---
+++ RootToLeafPathsSum/RootToLeafPathsSum.cpp
@@ -0,0 +1,154 @@
+/*
+https://www.geeksforgeeks.org/problems/root-to-leaf-paths-sum/1
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+
+class Solution {
+  public:
+  
+    int bfs(Node *root){
+        queue<pair<Node*, string>>q;
+        string bgn="";
+        bgn+=to_string(root->data);
+        q.push({root, bgn});
+        int res=0;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                auto curr=q.front();
+                q.pop();
+                Node *currnode=curr.first;
+                string currstr=curr.second;
+                if(currnode->left==nullptr&&currnode->right==nullptr){
+                    res+=stoi(currstr);
+                }
+                if(currnode->left!=nullptr){
+                    string nwstr=currstr;
+                    int tochk=stoi(nwstr);
+                    tochk=(10*tochk)+currnode->left->data;
+                    q.push({currnode->left, to_string(tochk)});
+                }
+                if(currnode->right!=nullptr){
+                    string nwstr=currstr;
+                    int tochk=stoi(nwstr);
+                    tochk=(10*tochk)+currnode->right->data;
+                    q.push({currnode->right, to_string(tochk)});
+                }
+            }
+        }
+        return res;
+    }
+  
+    int treePathsSum(Node *root) {
+        return bfs(root);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.treePathsSum(root);
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
---
+++ RootToLeafPathsSum/RootToLeafPathsSum.java
@@ -0,0 +1,171 @@
+/*
+https://www.geeksforgeeks.org/problems/root-to-leaf-paths-sum/1
+*/
+
+//{ Driver Code Starts
+// Contributed by Sudarshan Sharma
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution ob = new Solution();
+            System.out.println(ob.treePathsSum(root));
+            // System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below.
+Node is as follows:
+class Tree
+{
+      int data;
+      Tree left,right;
+      Tree(int d){
+          data=d;
+          left=null;
+          right=null;
+}
+}*/
+
+class Pair{
+    
+    public Node first;
+    public String second;
+    
+    Pair(Node first, String second){
+        this.first=first;
+        this.second=second;
+    }
+}
+
+class Solution {
+    
+    public static int bfs(Node root){
+        LinkedList<Pair>queue=new LinkedList<>();
+        String currentRootAsString=new String(String.valueOf(root.data));
+        queue.add(new Pair(root, currentRootAsString));
+        int resultantSum=0;
+        while(!queue.isEmpty()){
+            int sizeOfQueue=queue.size();
+            while(sizeOfQueue-- >0){
+                Pair currentPair=queue.pollLast();
+                Node currentNode=currentPair.first;
+                String currentRootAsStr=currentPair.second;
+                if(currentNode.left==null&&currentNode.right==null){
+                    resultantSum+=Integer.parseInt(currentRootAsStr);
+                }
+                if(currentNode.left!=null){
+                    String newStringToBeFormed=currentRootAsStr;
+                    int newSumToBeFormed=Integer.parseInt(newStringToBeFormed);
+                    newSumToBeFormed=(10*newSumToBeFormed)+currentNode.left.data;
+                    queue.add(new Pair(currentNode.left, String.valueOf(newSumToBeFormed)));
+                }
+                if(currentNode.right!=null){
+                    String newStringToBeFormed=currentRootAsStr;
+                    int newSumToBeFormed=Integer.parseInt(newStringToBeFormed);
+                    newSumToBeFormed=(10*newSumToBeFormed)+currentNode.right.data;
+                    queue.add(new Pair(currentNode.right, String.valueOf(newSumToBeFormed)));
+                }
+            }
+        }
+        return resultantSum;
+    }
+    
+    public static int treePathsSum(Node root) {
+        return bfs(root);
+    }
+}
